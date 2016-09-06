045e5e348bf2a9c408da13b7fa25bd3e641b0732
ruale7 <ruale7@gmail.com>
2016-09-06 16:40:46 +0200

    init

--- install_apache_2/metadata.rb
+++ install_apache_2/metadata.rb
@@ -5,9 +5,3 @@ license 'all_rights'
 description 'Installs/Configures install_apache_2'
 long_description 'Installs/Configures install_apache_2'
 version '0.1.0'
-
-depends 'poise-python'
-
-depends 'nginx'
-
-depends 'ruby', '~> 1.0.0'
